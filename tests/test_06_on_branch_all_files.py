1f2df0269a6d30ff1fa2fba5dea652ea81ce3f01
Denis Efremov <efremov@linux.com>
2021-10-02 16:08:59 +0300

    Remove DISPUTED CVE-2019-12382
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- tests/test_06_on_branch_all_files.py
+++ tests/test_06_on_branch_all_files.py
@@ -7,8 +7,6 @@ from cvehound.exception import UnsupportedVersion
 @pytest.mark.notbackported(
     ('cve', 'branch'),
     [
-        ('CVE-2019-12382', 'stable/linux-4.4.y'),
-        ('CVE-2019-12382', 'stable/linux-4.9.y'),
         ('CVE-2019-15924', 'stable/linux-4.4.y'),
         ('CVE-2020-27777', 'stable/linux-4.4.y'),
         ('CVE-2020-27777', 'stable/linux-4.9.y'),
